7cd169df72927d276a6639ca5c103a4c61a4702d
k03302 <unisonman660@gmail.com>
2023-12-25 12:48:40 +0900

    delete redundant files

--- mobile_robot_controller/info.py
+++
@@ -1,2 +0,0 @@
-__package_name__ = 'mobile_robot_controller'
-__version__ = '1.0.0'
--- mobile_robot_controller/mobile_robot_controller.py
+++
@@ -1,5 +0,0 @@
-def mobile_robot_controller():
-    pass
-
-if __name__ == '__main__':
-    mobile_robot_controller()
