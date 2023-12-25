71fb3e54f9518f348f63beae2a034e2462f6b357
k03302 <unisonman660@gmail.com>
2023-12-25 12:49:21 +0900

    add info files

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Mobile Robot Controller
 
 ### Introduce
+Team project of Software Engineering class in UOS
+Team members are composed of
+- Kim Hyuk Soon
+- Seok Jun Lee
+- Jun Young Heo
 
 ### How to install
+1. Move to the root folder
+2. Run commands below
+pip install -r requirements.txt
+python setup.py
 
 ### How to run
+from mrc.controller.worldController import MobileRobotController
+m = MobileRobotController()
+m.run()
 
 ### Reference
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
-from mobile_robot_controller.info import __package_name__, __version__
+from mrc.info import __package_name__, __version__
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
-with open('requirements.txt', 'r', encoding='utf-8') as f:
+with open("requirements.txt", "r", encoding="utf-8") as f:
     requires = f.read().splitlines()
-with open('test_requirements.txt', 'r', encoding='utf-8') as f:
+with open("test_requirements.txt", "r", encoding="utf-8") as f:
     test_requires = f.read().splitlines()
 
 
@@ -13,15 +13,15 @@ setup(
     name=__package_name__,
     version=__version__,
     long_description=readme,
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    package_data={'': ['*.yaml', '*.yml']},
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    package_data={"": ["*.yaml", "*.yml"]},
     install_requires=requires,
-    setup_requires=['pytest-runner'],
+    setup_requires=["pytest-runner"],
     tests_require=test_requires,
-    python_requires='>3.6',
+    python_requires=">3.6",
     classifiers=[
-        'Environment :: Console',
-        'Operating System :: POSIX :: LINUX',
-        'Programming Language :: Python :: 3.6'
-    ]
+        "Environment :: Console",
+        "Operating System :: POSIX :: LINUX",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
